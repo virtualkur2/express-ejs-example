ce3580cb5035ab6921cb45f2d9aaec20c5f5ad47
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-06-11 17:05:23 +0200

    added route to show getFile() method functionallity

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,4 +1,18 @@
+const path = require('path');
+const rootSendPath = path.join(__dirname,'../../public');
 const routes = (router) => {
+  router.route('/getfile')
+    .get((req,res,next) => {
+      const sendOptions = {
+        root: rootSendPath,
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      }
+      res.sendFile('js/getIP.js', sendOptions);
+    });
+
   router.route('/')
     .get((req, res, next) => {
       res.render('index', {pageTitle: 'Index page', title: 'The index page'});
